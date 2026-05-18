17a918b92dd23dd09d94a2cad1a069c3e08b45ec
Mohamed Khaled <mohammedk7alid2004@gmail.com>
2026-05-18 17:33:15 +0300

    add village crud

---
+++ BaridikExpress.Application/Features/LocationGeography/Commands/Villiage/UpdateToggleStatus/UpdateVillageToggleStatusCommand.cs
@@ -0,0 +1,6 @@
+﻿namespace BaridikExpress.Application.Features.LocationGeography.Commands.Villiage.UpdateToggleStatus;
+
+public class UpdateVillageToggleStatusCommand:IRequest<Result<bool>>
+{
+    public Guid Id { get; set; }
+}
---
+++ BaridikExpress.Application/Features/LocationGeography/Commands/Villiage/UpdateToggleStatus/UpdateVillageToggleStatusCommandHandler.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BaridikExpress.Application.Features.LocationGeography.Commands.Villiage.UpdateToggleStatus;
+
+public class UpdateVillageToggleStatusCommandHandler(
+    IApplicationDbContext application,
+    IStringLocalizer localizer)
+    : IRequestHandler<UpdateVillageToggleStatusCommand, Result<bool>>
+{
+    private readonly IApplicationDbContext _application = application;
+    private readonly IStringLocalizer _localizer = localizer;
+
+    public async Task<Result<bool>> Handle(
+        UpdateVillageToggleStatusCommand request,
+        CancellationToken cancellationToken)
+    {
+        var village = await _application.Villages
+            .FirstOrDefaultAsync(
+                x => x.VillageId == request.Id,
+                cancellationToken);
+
+        if (village is null)
+        {
+            return Result<bool>
+                .Failure(_localizer["VillageNotFound"], 404);
+        }
+
+        village.ToggleStatus();
+
+        await _application.SaveChangesAsync(cancellationToken);
+
+        var message = village.IsActive
+            ? _localizer["VillageActivatedSuccessfully"]
+            : _localizer["VillageDeactivatedSuccessfully"];
+
+        return Result<bool>.Success(true, message);
+    }
+}
