1f83d9f2d48988b2556f5bd2352b305fb4a776b8
Mohamed Khaled <mohammedk7alid2004@gmail.com>
2026-06-02 10:22:28 +0300

    add create shipment

---
+++ BaridikExpress.API/Controllers/ClientModule/ShipmentsController.cs
@@ -0,0 +1,29 @@
+﻿using BaridikExpress.Application.Features.Shipments.Commands.CreateShipment;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BaridikExpress.API.Controllers.AuthClientModule;
+
+[ApiController]
+[Route("api/client/[controller]")]
+[Authorize]
+[ApiExplorerSettings(GroupName = "client-v1")]
+[Tags("Shipments")]
+public class ShipmentsController(ISender sender) : ControllerBase
+{
+    private readonly ISender _sender = sender;
+
+    [HttpPost]
+    [Consumes("multipart/form-data")]
+    public async Task<IActionResult> Create(
+        [FromForm] CreateShipmentCommand command, CancellationToken cancellationToken)
+    {
+        var result = await _sender.Send(command, cancellationToken);
+
+        return result.IsSuccess
+            ? StatusCode(StatusCodes.Status201Created, result)
+            : StatusCode(result.StatusCode, result);
+    }
+}
