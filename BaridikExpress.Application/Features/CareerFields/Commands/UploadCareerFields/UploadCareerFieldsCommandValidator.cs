cbc89d7946c6e0b5f274fa554d045ffb642e3135
Ahmed Momtaz Mohamed <am7883@fayoum.edu.eg>
2026-06-04 16:32:12 +0300

    Add validation for UploadValidator

---
+++ BaridikExpress.Application/Features/CareerFields/Commands/UploadCareerFields/UploadCareerFieldsCommandValidator.cs
@@ -0,0 +1,31 @@
+﻿using ServiceStack;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaridikExpress.Application.Features.CareerFields.Commands.UploadCareerFields
+{
+    public class UploadCareerFieldsCommandValidator:AbstractValidator<UploadCareerFieldsCommand>
+    {
+        public UploadCareerFieldsCommandValidator(IStringLocalizer localizer)
+        {
+
+            RuleFor(x => x.File)
+              .NotNull()
+              .WithMessage(localizer["ExcelFileRequired"]);
+
+        RuleFor(x => x.File)
+              .Must(file =>
+              {
+            var extension = Path.GetExtension(file!.FileName)
+                .ToLowerInvariant();
+
+            return extension is ".xlsx" or ".xls";
+        })
+              .WithMessage(localizer["ExcelInvalidType"])
+              .When(x => x.File != null);
+    }
+}
+}
