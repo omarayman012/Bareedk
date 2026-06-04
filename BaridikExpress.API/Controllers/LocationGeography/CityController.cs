ede3b9847aeca18f70339446648b70929c99b6dc
Mohamed Khaled <mohammedk7alid2004@gmail.com>
2026-06-04 19:09:23 +0300

    Rafactor For controller city

--- BaridikExpress.API/Controllers/LocationGeography/CityController.cs
+++ BaridikExpress.API/Controllers/LocationGeography/CityController.cs
@@ -11,86 +11,76 @@ using Microsoft.AspNetCore.Mvc;
 namespace BaridikExpress.API.Controllers.LocationGeography;
 
 [ApiController]
-[Route("api/[controller]")]
+[Route("api/v1/[controller]")]
 [Authorize]
 [ApiExplorerSettings(GroupName = "admin-v1")]
 [Tags("Cities")]
 public class CityController(IMediator mediator) : ControllerBase
 {
-    private readonly IMediator _mediator = mediator;
-
-    // GET: api/city?pageNumber=1&pageSize=10
     [HttpGet]
-    public async Task<IActionResult> GetAll(
-        [FromQuery] GetAllCityQuery query)
+    public async Task<IActionResult> GetAll([FromQuery] GetAllCityQuery query)
     {
-        var result = await _mediator.Send(query);
+        var result = await mediator.Send(query);
 
         return result.IsSuccess
             ? Ok(result)
             : StatusCode(result.StatusCode, result);
     }
 
-    // GET: api/city/{id}
     [HttpGet("{id:guid}")]
     public async Task<IActionResult> GetById(Guid id)
     {
-        var result = await _mediator.Send(
-            new GetCityByIdQuery
-            {
-                Id = id
-            });
+        var result = await mediator.Send(new GetCityByIdQuery
+        {
+            Id = id
+        });
 
         return result.IsSuccess
             ? Ok(result)
             : StatusCode(result.StatusCode, result);
     }
 
-    // POST: api/city
     [HttpPost]
-    public async Task<IActionResult> Create(
-        [FromBody] CreateCityCommand command)
+    public async Task<IActionResult> Create([FromBody] CreateCityCommand command)
     {
-        var result = await _mediator.Send(command);
+        var result = await mediator.Send(command);
 
         return result.IsSuccess
             ? StatusCode(StatusCodes.Status201Created, result)
             : StatusCode(result.StatusCode, result);
     }
 
-    // PUT: api/city
-    [HttpPut]
+    [HttpPut("{id:guid}")]
     public async Task<IActionResult> Update(
-        [FromBody] UpdateCityCommand command)
+     Guid id,
+     [FromBody] UpdateCityCommand command)
     {
-        var result = await _mediator.Send(command);
+        command.Id = id;
+
+        var result = await mediator.Send(command);
 
         return result.IsSuccess
             ? Ok(result)
             : StatusCode(result.StatusCode, result);
     }
 
-    // DELETE: api/city
     [HttpDelete]
-    public async Task<IActionResult> Delete(
-        [FromBody] DeleteCityCommand command)
+    public async Task<IActionResult> Delete([FromBody] DeleteCityCommand command)
     {
-        var result = await _mediator.Send(command);
+        var result = await mediator.Send(command);
 
         return result.IsSuccess
             ? Ok(result)
             : StatusCode(result.StatusCode, result);
     }
 
-    // PATCH: api/city/toggle-status/{id}
     [HttpPatch("toggle-status/{id:guid}")]
     public async Task<IActionResult> ToggleStatus(Guid id)
     {
-        var result = await _mediator.Send(
-            new UpdateCityToggleStatusCommand
-            {
-                Id = id
-            });
+        var result = await mediator.Send(new UpdateCityToggleStatusCommand
+        {
+            Id = id
+        });
 
         return result.IsSuccess
             ? Ok(result)
